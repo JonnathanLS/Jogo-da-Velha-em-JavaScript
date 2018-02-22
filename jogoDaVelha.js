cf665be3466989232b983d91a3a232497a4e5db9
Jonnathan Lacerda Santos <jonnathan.lacerda.santos@gmail.com>
2018-02-22 17:42:25 -0300

    Implementado função para Reiniciar o Jogo

--- jogoDaVelha.js
+++ jogoDaVelha.js
@@ -1,27 +1,37 @@
 var Jogador = null;
-var TabuleiroJogo = [
-					[null,null,null],
-					[null,null,null],
-					[null,null,null]
-				]
+var TabuleiroJogo = [[null,null,null],[null,null,null],[null,null,null]];
 
 function IniciarJogo(){
 
+	document.getElementById('IniciarJogo').style.display = 'none';
+	document.getElementById('ReiniciarJogo').style.display = 'block';
 	document.getElementById('EscolherXO1').style.display = 'block';
 	document.getElementById('EscolherXO2').style.display = 'block';
 	
-	document.getElementById('CorpoMensagem').innerHTML = "1º Jogador escolha se você quer ser o 'X' ou 'O' (clique na Opção Abaixo)!!!"
-		
-
-	
-	
-	
+	document.getElementById('CorpoMensagem').innerHTML = "1º Jogador escolha se você quer ser o 'X' ou 'O' (clique na Opção Abaixo)!!!";	
 
 }
 function MudarEstado(elemento){
 	var tag = document.getElementById(elemento).style.display;
 }
 
+function ReiniciarJogo(){
+	TabuleiroJogo = [[null,null,null],[null,null,null],[null,null,null]];
+	document.getElementById('p0').innerHTML = "";
+	document.getElementById('p1').innerHTML = "";
+	document.getElementById('p2').innerHTML = "";
+	document.getElementById('p3').innerHTML = "";
+	document.getElementById('p4').innerHTML = "";
+	document.getElementById('p5').innerHTML = "";
+	document.getElementById('p6').innerHTML = "";
+	document.getElementById('p7').innerHTML = "";
+	document.getElementById('p8').innerHTML = "";
+	document.getElementById('Resultado').style.display = 'none';
+	if (VerificarJogo() == undefined){
+		document.getElementById('CorpoMensagem').innerHTML = "";
+		document.getElementById('CorpoJogo').style.display = 'block';
+	}
+}
 function FirstPlayer(opcao){	
 	Jogador = opcao;
 	document.getElementById('EscolherXO1').style.display = 'none';
@@ -46,11 +56,17 @@ function AtualizarMatriz(linha, coluna, id, elemento){
 		TabuleiroJogo[linha][coluna] = elemento;	
 		document.getElementById(id).innerHTML = Jogador;
 		
-		if (VerificarJogo() == true){
+		if (VerificarJogo() == 1){
+
 			document.getElementById('CorpoJogo').style.display = 'none';
 			document.getElementById('CorpoMensagem').innerHTML = Jogador + " Ganhou!!!"
 			document.getElementById('Resultado').style.display = 'block';
 		}
+		else if (VerificarJogo() == 0) {
+			document.getElementById('CorpoJogo').style.display = 'none';
+			document.getElementById('Resultado').style.display = 'block';
+		}
+
 		TrocarJogador(Jogador);
 	}
 	else{
@@ -74,47 +90,47 @@ function VerificarJogo(){
 	// Verifica o jogo na Horizontal
 	// Linha 1
 	var status = (p0 == p1 && p1 == p2 && p0 != null && p1 != null && p2 != null);	
-	if (status == true){
-		return true;
+	if (status == 1){
+		return 1;
 	}
 	// Linha 2
-	var status = (p3 == p4 && p4 == p5 && p3 != null && p4 != null && p5 != null);	
+	status = (p3 == p4 && p4 == p5 && p3 != null && p4 != null && p5 != null);	
 	if (status == true){
-		return true;
+		return 1;
 	}
 	// Linha 3
-	var status = (p6 == p7 && p7 == p8 && p6 != null && p7 != null && p8 != null);	
+	status = (p6 == p7 && p7 == p8 && p6 != null && p7 != null && p8 != null);	
 	if (status == true){
-		return true;
+		return 1;
 	}
 
 	// Verifica o Jogona Vertical
 	// Coluna 1
-	var status = (p0 == p3 && p3 == p6 && p0 != null && p3 != null && p6 != null);	
+	status = (p0 == p3 && p3 == p6 && p0 != null && p3 != null && p6 != null);	
 	if (status == true){
-		return true;
+		return 1;
 	}
 	// Coluna 2
-	var status = (p1 == p4 && p4 == p7 && p1 != null && p4 != null && p7 != null);	
+	status = (p1 == p4 && p4 == p7 && p1 != null && p4 != null && p7 != null);	
 	if (status == true){
-		return true;
+		return 1;
 	}
 	// Coluna 3
-	var status = (p2 == p5 && p5 == p8 && p2 != null && p5 != null && p8 != null);	
+	status = (p2 == p5 && p5 == p8 && p2 != null && p5 != null && p8 != null);	
 	if (status == true){
-		return true;
+		return 1;
 	}
 
 	// Verifica o Jogo na Trasnversal
 	// Transversal 1 
-	var status = (p0 == p4 && p4 == p8 && p0 != null && p4 != null && p8 != null);	
+	status = (p0 == p4 && p4 == p8 && p0 != null && p4 != null && p8 != null);	
 	if (status == true){
-		return true;
+		return 1;
 	}
 	// Trasnversal 2
-	var status = (p2 == p4 && p4 == p6 && p2 != null && p4 != null && p6 != null);	
+	status = (p2 == p4 && p4 == p6 && p2 != null && p4 != null && p6 != null);	
 	if (status == true){
-		return true;
+		return 1;
 	}
 	// Verifica se deu velha 
 	if (p0 != null &&
@@ -128,7 +144,7 @@ function VerificarJogo(){
 		p8 != null
 		){
 		document.getElementById('CorpoMensagem').innerHTML = "Deu Velha";
-		return true;
+		return 0;
 	}
 }
 
